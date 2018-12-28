e6c933c31118fbd09d40ad16a716fe64ebb2e939
anagornyuk <anagorniuk@adyax.com>
2018-12-28 13:30:57 +0200

    Added some validators to inputs

--- Andrii/scripts/script.js
+++ Andrii/scripts/script.js
@@ -6,6 +6,7 @@ function ShowLogin(){
 	document.getElementById('login').classList.remove("unchecked");
 	document.getElementById('login').classList.add("checked");
 }
+
 function ShowSignUp(){
 	document.getElementById('signUpForm').className = "visible";
 	document.getElementById('loginForm').className = "hidden";
@@ -15,9 +16,58 @@ function ShowSignUp(){
 	document.getElementById('signup').classList.add("checked");
 	
 }
+
+function ValidateName(id){
+	let regexp = /^\D+$/;
+	if (regexp.test(id.value) != true){
+		ShowTip(nametip);
+		id.style.borderColor = "red";
+	}
+	if (regexp.test(id.value) == true){
+		HideTip(nametip);
+		id.style.borderColor = "green";
+	}
+	if (id.value == ""){
+		id.style.borderColor = "initial";
+	}
+} 
+
+function ValidateEmail(id){
+	let emailre = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if (id.value == ""){
+		id.style.borderColor = "initial";
+	}
+	if (emailre.test(id.value) != true){
+		//ShowTip(nametip);
+		id.style.borderColor = "yellow";
+	}
+	if (emailre.test(id.value) == true){
+		//HideTip(nametip);
+		id.style.borderColor = "green";
+	}
+}
+
+function OnEmailLeaveFocus (id) {
+	let emailre = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if (emailre.test(id.value) != true){
+		ShowTip(emailtip);
+		id.style.borderColor = "red";
+	}
+	if (emailre.test(id.value) == true){
+		HideTip(emailtip);
+		id.style.borderColor = "green";
+	}
+	if (id.value == ""){
+		id.style.borderColor = "initial";
+		HideTip(emailtip);
+	}
+
+}
+
 function ValidatePassword(){
 	let pswtip = document.getElementById('pswtip');
-	let regexp = /\w{6,}/
+	let regexp = /\w{6,}/;
+	
 	pswtip.className = "visible";
 	if (regexp.test(document.getElementById('password').value)){
 		pswtip.className = "hidden";
