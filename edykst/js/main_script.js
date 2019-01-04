dd65984fc5ee9e4523dded8673d3b8130571732d
edykst <edykst@gmail.com>
2019-01-04 18:12:07 +0200

    Add botton on main page, file with form, loading page on AJAX method

--- edykst/js/main_script.js
+++ edykst/js/main_script.js
@@ -1,28 +1,52 @@
 function showHide(element_id) {
 
-    but_act = document.querySelector('.div_input');
-
-    if(element_id.indexOf('blok') + 1)
-        (element_id !== "blok_js")?(hide_blok = "blok_js"):(hide_blok = "blok_css");
-    else if(element_id.indexOf('form') + 1)
-        if(element_id !== "form_login") {
-            hide_blok = "form_login";
+    if (element_id.indexOf('blok') + 1){
+        switch (element_id) {
+            case 'blok_js':
+                hide_blok_one = "blok_css";
+                hide_blok_two = "blok_ajax";
+                break;
+            case 'blok_css':
+                hide_blok_one = "blok_js";
+                hide_blok_two = "blok_ajax";
+                break;
+            case 'blok_ajax':
+                hide_blok_one = "blok_css";
+                hide_blok_two = "blok_js";
+                break;
+        }
+        document.getElementById(hide_blok_one).style.display = "none";
+        document.getElementById(hide_blok_two).style.display = "none";
+    }
+    else if(element_id.indexOf('form') + 1) {
+        but_act = document.querySelector((element_id.indexOf('ajax') + 1) ? '#but_ajax' : '#but_js');
+        if ((element_id !== "form_login") && (element_id !== "form_login_ajax")) {
+            document.getElementById((element_id.indexOf('ajax') + 1) ? 'form_login_ajax' : 'form_login').style.display = "none";
             but_act.children[0].classList.add("input_inactive");
             but_act.children[1].classList.add("input_active");
             but_act.children[0].classList.remove("input_active");
             but_act.children[1].classList.remove("input_inactive");
         } else {
-            hide_blok = "form_sign";
+            document.getElementById((element_id.indexOf('ajax') + 1) ? 'form_sign_ajax' : 'form_sign').style.display = "none";
             but_act.children[0].classList.add("input_active");
             but_act.children[1].classList.add("input_inactive");
             but_act.children[0].classList.remove("input_inactive");
             but_act.children[1].classList.remove("input_active");
         }
+    }
 
     if (document.getElementById(element_id)) {
         var obj = document.getElementById(element_id);
         if (obj.style.display !== "block") {
             obj.style.display = "block";
-            document.getElementById(hide_blok).style.display = "none"}
+        }
     }
 }
+
+$(document).ready(function() {
+    $('#ajax').click(function() {
+        showHide('blok_ajax');
+        $('#blok_ajax').load('form_js.html');
+        $('#blok_ajax').css('display', 'block');
+    })
+});
