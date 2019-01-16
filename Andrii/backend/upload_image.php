886f0623f4d160d5dbf15399632203e82f271254
anagornyuk <anagorniuk@adyax.com>
2019-01-16 13:46:05 +0200

    Code refractoring

--- Andrii/backend/upload_image.php
+++ Andrii/backend/upload_image.php
@@ -5,36 +5,39 @@ require 'database_connection.php';
 $uploaddir = '../images/';
 //$uploadfile = $uploaddir . basename($_FILES['upload']['name']);
 
-if (is_uploaded_file($_FILES['upload']['tmp_name'])){
-    if (getimagesize($_FILES['upload']['tmp_name'])[mime] == 'image/jpeg' ||
-        getimagesize($_FILES['upload']['tmp_name'])[mime] == 'image/png'){
-        $name = md5_file($_FILES['upload']['tmp_name']);
-        $extension = image_type_to_extension(getimagesize($_FILES['upload']['tmp_name'])[2]);
-        $filename = $name . $extension;
-        $uploadfile = $uploaddir . $name . $extension;
-        if (move_uploaded_file($_FILES['upload']['tmp_name'], $uploadfile)) {
-            chmod($imageFullName, 0777);
-            //write to db
-            session_start();
-            if(!isset($_SESSION['id'])) {
-                echo "Unable define user";
-                exit();
-            }
-            $sql_update = "UPDATE users SET Image=? WHERE id=?";
-            try {
-                $conn->prepare($sql_update)->execute([$filename, $_SESSION['id']]);
-                echo "New record created successfully";
-            }
-            catch(PDOException $e) {
-                echo $e->getMessage();
-            }
+if (is_uploaded_file($_FILES['upload']['error']) == '0') {
+    if (is_uploaded_file($_FILES['upload']['tmp_name'])){
+        if (getimagesize($_FILES['upload']['tmp_name'])[mime] == 'image/jpeg' ||
+            getimagesize($_FILES['upload']['tmp_name'])[mime] == 'image/png'){
+            $name = md5_file($_FILES['upload']['tmp_name']);
+            $extension = image_type_to_extension(getimagesize($_FILES['upload']['tmp_name'])[2]);
+            $filename = $name . $extension;
+            $uploadfile = $uploaddir . $name . $extension;
+            if (move_uploaded_file($_FILES['upload']['tmp_name'], $uploadfile)) {
+                chmod($imageFullName, 0777);
+                //write to db
+                session_start();
+                if(!isset($_SESSION['id'])) {
+                    echo "Unable define user";
+                    exit();
+                }
+                $sql_update = "UPDATE users SET Image=? WHERE id=?";
+                try {
+                    $conn->prepare($sql_update)->execute([$filename, $_SESSION['id']]);
+                    echo "New record created successfully";
+                }
+                catch(PDOException $e) {
+                    echo $e->getMessage();
+                }
 
-            echo "File was uploaded";
-            header('Location: ../userpage.html.php');
+                echo "File was uploaded";
+                header('Location: ../userpage.html.php');
+            } else {
+                echo "Upload error";
+            }
         } else {
-            echo "Upload error";
+            echo "File type must be only image!";
         }
-    } else {
-        echo "File type must be only image!";
     }
+     echo "Upload error";
 }
--- Andrii/userpage.html.php
+++ Andrii/userpage.html.php
@@ -7,88 +7,15 @@
     <link rel="stylesheet" type="text/css" href="styles/userpage.css">
 </head>
 <body>
-    <div>
+    <div class="cont">
         <?php
-            session_start();
-            //echo "hello";
-            //echo PHP_EOL;
-            $fname = $_SESSION["username"];
-            if(!isset($_SESSION['username'])) {
-                echo "Please log in or sign up";
-                exit();
-            }
-            $uid = $_SESSION["id"];
-            if(!isset($_SESSION['id'])) {
-                echo "Please log in or sign up";
-                exit();
-            }
-            $infotable = '
-                <div>
-                    <table>
-                        <tr>
-                            <td>First name</td>
-                            <td id="table_fname">%s</td>
-                            <td id="table_fname_button"><button onclick="changeField(table_fname, table_fname_button)">Change</button></td>
-                        </tr>
-                        <tr>
-                            <td>Last name</td>
-                            <td id="table_lname">%s</td>
-                            <td id="table_lname_button"><button onclick="changeField(table_lname, table_lname_button)">Change</button></td>
-                        </tr>
-                        <tr>
-                            <td>Email</td>
-                            <td id="table_email">%s</td>
-                            <td id="table_email_button"><button onclick="changeField(table_email, table_email_button)">Change</button></td>
-                        </tr>
-                        <tr>
-                            <td>Password</td>
-                            <td id="table_password"><button onclick="showPassword()">Show</button></td>
-                            <td id=""><a href="changePassword.html">Change</a></td>
-                        </tr>
-                    </table>
-                </div>
-            ';
-            //echo $fname;
-            if ($uid){
-                include_once 'backend/database_connection.php';
-                //echo $conn;
-                $stmt = $conn->prepare("SELECT * FROM users WHERE id = ? ");
-                $stmt->execute([$uid]);
-                $select = $stmt->fetch();
-                echo("<p> Hello, <span style='color: fuchsia'>" . $select['FirstName'] . "</span>!</p>");
-    //            echo("<div><table>");
-    //            echo("<tr><td>First name</td><td>" . $select['FirstName'] . "</td></tr>");
-    //            echo("<tr><td>Last name</td><td>" . $select['LastName'] . "</td></tr>");
-    //            echo("<tr><td>Email</td><td>" . $select['Email'] . "</td></tr>");
-                echo sprintf($infotable, $select['FirstName'],  $select['LastName'], $select['Email']);
-    //            foreach ($select as $key => $value) {
-    //                echo "{$key} => {$value}"."<br />\n";
-    //            }
-
-
-            }
+            require 'backend/userpage.php';
+            $uid = is_sess();
+            $select = db_select($uid);
+            print_hello($select);
+            print_userlogo($select);
+            print_creditals($select);
         ?>
-    </div>
-    <div>
-        <?php
-        $image = '<img src="images/%s" alt="" style="width: 400px; height: 400px">';
-        if ($select['Image'] == 'null') {
-            echo sprintf($image, 'logo.png');
-        } else {
-            echo sprintf($image, $select['Image']);
-        }
-
-        ?>
-    </div>
-    <form method="post" enctype="multipart/form-data" action="backend/upload_image.php" >
-        <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-        <input type="file" name="upload">
-        <input type="submit" value="Load image">
-    </form>
-    <div>
-        <form method="get" action="backend/logout.php">
-            <input type="submit" value="Log out">
-        </form>
     </div>
 </body>
 </html>
