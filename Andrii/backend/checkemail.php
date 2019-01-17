4a52a350f3b95fe3d69f9171a7fe66452a92cf81
anagornyuk <anagorniuk@adyax.com>
2019-01-17 18:46:36 +0200

    checkemail fix

--- Andrii/backend/checkemail.php
+++ Andrii/backend/checkemail.php
@@ -1,7 +1,20 @@
 <?php
 
-require 'database_connection.php';
 
+require 'evn_vars.php';
+try {
+    $conn = new PDO("mysql:host=$servername", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected to host successfully \n";
+    $conn->query("CREATE DATABASE IF NOT EXISTS $dbname");
+    $conn->query("use $dbname");
+    //echo "Connected to database successfully \n";
+    $conn->query($table);
+
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 $select = $conn->query("SELECT Email FROM users;")->fetchAll();
 foreach ($select as $row) {
     //echo $row[0]."\n";
